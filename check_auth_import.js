25c7fc76fe41a0a86dfd7fb238e514a0d9c5c3f4
bentansusanto <tansusanto194@gmail.com>
2026-04-27 11:00:53 +0700

    chore: remove temporary and test files accidentally pushed

--- check_auth_import.js
+++
@@ -1,3 +0,0 @@
-
-const { AuthService } = require('./src/modules/rbac/auth/auth.service');
-console.log('AuthService:', AuthService);
--- test-logout.js
+++
@@ -1,3 +0,0 @@
-const http = require('http');
-
-const data = JSON.stringify({ email: 'bennytansusanto@gmail.com', password: 'password123' }); // I don't know the exact password, let's see. 
